2b0fdd59ed4674e4a9e08267e04c3717c3d1dad9
Ilya_Shipilov <ilya.shipilov2017@mail.ru>
2022-12-17 18:38:13 +0300

    Programm create

---
+++ seminar/programm1/Program.cs
@@ -0,0 +1,21 @@
+﻿//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.Clear();
+int [] number = new int [2];
+string [] title = {"первое", "второе"};
+int   ;
+while (count < number.Length){
+    Console.Write($"Введите {title[count]} число: ");
+    number[count] = int.Parse(Console.ReadLine;
+    count++;
+}
+int max = number[0];
+count = 0;
+while(count < number.Length)
+{
+    if(max < number[count]){ max = number[count]; }
+    count++;
+}
+Console.WriteLine($"Максимальное число равно: {max}");
